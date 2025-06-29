1541ca3ee849d747d54967cf697bed92da9d97a0
Maarten Nieber <hallomaarten@yahoo.com>
2025-06-29 20:08:33 +0200

    Fix normalize_path

--- src/lcopy/files/utils/normalize_path.py
+++ src/lcopy/files/utils/normalize_path.py
@@ -7,7 +7,7 @@ def normalize_path(rel_path: str, base_path: str = "") -> str:
     expanded_rel = os.path.expandvars(os.path.expanduser(rel_path))
 
     # Join with relative path if provided
-    if base_path:
+    if base_path and not os.path.isabs(expanded_rel):
         expanded_base = os.path.expandvars(os.path.expanduser(base_path))
         path = os.path.join(expanded_base, expanded_rel)
     else:
